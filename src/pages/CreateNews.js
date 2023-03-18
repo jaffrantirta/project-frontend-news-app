41083fffc5ce0e721a71a32430fa985ed4985bd1
jaffrantirta <franartika@gmail.com>
2023-03-19 01:12:53 +0800

    store berita

--- src/pages/CreateNews.js
+++ src/pages/CreateNews.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ButtonComponent, InputComponent } from '../components'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { ERROR_MESSAGE, SUPABASE_KEY, SUPABASE_URL } from '../utils/Constant';
 import Swal from 'sweetalert2';
 import { createClient } from '@supabase/supabase-js';
+import Loader from '../utils/Loader';
 
 
 export default function CreateNews() {
@@ -12,58 +13,89 @@ export default function CreateNews() {
     const [content, setContent] = useState('');
     const [title, setTitle] = useState('')
     const [file, setFile] = useState(null)
+    const [categories, setCategories] = useState([])
+    const [categorySelected, setCategorySelected] = useState(0)
+    const [tags, setTags] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
 
     const onChange = (newContent) => {
         setContent(newContent);
     }
 
-    const handleOnChangeIamge = async (event) => {
-        const file = event.target.files[0];
-        const { data, error } = await supabase.storage.from('images').upload(`${file.name}`, file);
-
-        if (error) {
-            throw error;
+    const store = async (e) => {
+        if (file === null) {
+            Swal.fire('', 'Masukan gambar!', 'error')
+            throw null
         }
-        console.log(data);
-    }
-
-    const uploadImage = async (file) => {
-        const { data, error } = await supabase.storage.from('images').upload(`${file.name}`, file, {
+        if (categorySelected === 0) {
+            Swal.fire('', 'Pilih kategori!', 'error')
+            throw null
+        }
+        setIsLoading(true)
+        const { data, error } = await supabase.storage.from('images').upload(`${Date.now()}${file.name}`, file, {
             cacheControl: '3600',
             upsert: false
-        });
+        })
+        if (error !== null) {
+            Swal.fire(ERROR_MESSAGE, error.message, 'error')
+            throw error
+        }
+        const { error: errorNews } = await supabase
+            .from('news')
+            .insert([
+                { title: title, tags: tags, content: content, category_id: categorySelected, image: data.path },
+            ])
+
+        if (errorNews !== null) {
+            Swal.fire(ERROR_MESSAGE, errorNews.message, 'error')
+            throw errorNews
+        }
+        clear()
+        Swal.fire('Berhasil', 'Berita telah ditambahkan', 'success')
+        setIsLoading(false)
+    }
+
+    const getCategories = async () => {
+        setIsLoading(true)
+        let { data: categoriesList, error } = await supabase
+            .from('categories')
+            .select('*')
+            .order('name')
 
         if (error) {
-            throw error;
+            Swal.fire(ERROR_MESSAGE, error.message, 'error')
+            throw error
         }
 
-        return data.Key;
+        setCategories(categoriesList)
+        setIsLoading(false)
+
     }
 
-    const store = async (e) => {
-        uploadImage(file).then(async imageName => {
-            const { error } = await supabase
-                .from('news')
-                .insert([
-                    { title: title, content: content, image: imageName },
-                ])
+    useEffect(() => {
+        getCategories()
+    }, [])
 
-            if (!error) {
-                Swal.fire('Berhasil', 'Berita telah ditambahkan', 'success')
-            }
-            Swal.fire(ERROR_MESSAGE, error, 'error')
-        })
+    const clear = () => {
+        setContent('')
+        setTitle('')
+        setTags('')
+        setFile(null)
     }
 
 
-
-    return (
+    return (isLoading ? <Loader loadText={'Mohon tunggu...'} /> :
         <div>
             <p>Buat Berita</p>
             <div className='flex flex-col'>
+                <select className='my-5' onChange={e => setCategorySelected(e.target.value)}>
+                    <option disabled>- Pilih kategori -</option>
+                    {categories.map((item, index) => <option key={index} value={item.id}>{item.name}</option>)}
+                </select>
                 <InputComponent onChange={e => setTitle(e.target.value)} value={title} name={'title'} placeholder='Judul' />
                 <ReactQuill className='h-64 mt-10' value={content} onChange={onChange} />
-                <InputComponent className={'mt-20'} type={'file'} onChange={e => handleOnChangeIamge(e)} />
+                <InputComponent className={'mt-20'} type={'file'} onChange={e => setFile(e.target.files[0])} />
+                <InputComponent onChange={e => setTags(e.target.value)} value={tags} name={'tags'} placeholder='Tags' />
                 <ButtonComponent onClick={() => store()} text={'Simpan'} />
             </div>
         </div>
