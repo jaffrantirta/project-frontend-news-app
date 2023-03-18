3fb81aed6f8108144d2ff624d597b53772bf4aca
jaffrantirta <franartika@gmail.com>
2023-03-18 12:39:17 +0800

    category ok

--- src/components/InputComponent.js
+++ src/components/InputComponent.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function InputComponent({ name, autoFocus = false, placeholder, type, inputMode, className }) {
+export default function InputComponent({ name, value, autoFocus = false, placeholder, type, inputMode, className }) {
     return (
-        <input autoFocus={autoFocus} className={`p-3 my-1 rounded-full focus:border-primary border border-slate-500 ${className}`} type={type} inputMode={inputMode} name={name} placeholder={placeholder} />
+        <input autoFocus={autoFocus} value={value} className={`p-3 my-1 rounded-full focus:border-primary border border-slate-500 ${className}`} type={type} inputMode={inputMode} name={name} placeholder={placeholder} />
     )
 }
--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,60 +1,126 @@
 import { createClient } from '@supabase/supabase-js';
-import { Modal } from 'flowbite-react';
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import { ButtonComponent, InputComponent, TableComponent } from '../components';
+import { ButtonComponent, TableComponent } from '../components';
 import { ERROR_MESSAGE, SUPABASE_KEY, SUPABASE_URL } from '../utils/Constant';
 import Loader from '../utils/Loader';
 
 export default function Category() {
-    const supabaseUrl = SUPABASE_URL
-    const supabaseKey = SUPABASE_KEY
-    const supabase = createClient(supabaseUrl, supabaseKey)
-    const [data, setData] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-    const [modalShow, setModalShow] = useState(false)
-    const formRef = useRef(null)
+    const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+    const [categoriesList, setCategoriesList] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
     const heads = [{ title: 'No.' }, { title: 'Nama Kategori' }, { title: 'Aksi' }]
 
-    const addCategory = async (name) => {
-        setIsLoading(true)
-        const { error } = await supabase
-            .from('categories')
-            .insert([
-                { name: name },
-            ])
+    const createCategory = async () => {
+        Swal.fire({
+            input: 'text',
+            inputLabel: 'Nama kategori',
+            inputPlaceholder: 'Masukan nama kategori',
+            confirmButtonText: 'Tambah',
+            showCancelButton: true,
+            showLoaderOnConfirm: true,
+            preConfirm: async (value) => {
+                const { error } = await supabase
+                    .from('categories')
+                    .insert([{ name: value }])
 
-        if (error) {
-            Swal.fire(ERROR_MESSAGE, error, 'error')
-        } else {
-            setModalShow(false)
-            getCategories()
-            Swal.fire('Berhasil', 'Kategori ditambahkan', 'success')
-        }
-        setIsLoading(false)
+                if (!error) {
+                    Swal.hideLoading()
+                    return true
+                }
+                Swal.showValidationMessage(ERROR_MESSAGE)
+            }
+        }).then(async response => {
+            if (response.isConfirmed) {
+                Swal.fire('Berhasil', 'Kategori ditambahkan', 'success')
+                getCategories()
+            }
+
+        })
+    }
+    const deleteCategory = (id, name) => {
+        Swal.fire({
+            title: `Yakin hapus kategori ${name}`,
+            icon: 'question',
+            confirmButtonText: 'Hapus',
+            confirmButtonColor: 'red',
+            showCancelButton: true,
+            showLoaderOnConfirm: true,
+            preConfirm: async () => {
+                const { error } = await supabase
+                    .from('categories')
+                    .delete()
+                    .eq('id', id)
+
+                if (!error) {
+                    Swal.hideLoading()
+                    return true
+                }
+                Swal.showValidationMessage(ERROR_MESSAGE)
+            }
+        }).then(response => {
+            if (response.isConfirmed) {
+                Swal.fire('Berhasil', `Kategori ${name} telah dihapus`, 'success')
+                getCategories()
+            }
+        })
     }
     const getCategories = async () => {
         setIsLoading(true)
         let { data: categories, error } = await supabase
             .from('categories')
             .select('*')
+            .order('created_at', { ascending: false })
 
         if (error) {
             Swal.fire(ERROR_MESSAGE, error, 'error')
         } else {
-            setData(categories.map((item, index) => {
+            setCategoriesList(categories.map((item, index) => {
                 return (
                     <tr key={item.id}>
                         <td className="p-3 border border-slate-500 text-center">{index + 1}</td>
                         <td className="p-3 border border-slate-500">{item.name}</td>
-                        <td className="p-3 border border-slate-500 text-center"><span>edit</span></td>
+                        <td className="p-3 border border-slate-500 text-center">
+                            <Link className={'m-1 hover:text-slate-700 text-orange-500'} onClick={() => editCategory(item.id, item.name)} >Edit</Link>
+                            <Link className={'m-1 hover:text-slate-700 text-red-500'} onClick={() => deleteCategory(item.id, item.name)} >Hapus</Link>
+                        </td>
                     </tr>
                 )
             }))
         }
         setIsLoading(false)
     }
-    // eslint-disable-next-line
+
+    const editCategory = (id, name) => {
+        Swal.fire({
+            input: 'text',
+            inputLabel: 'Nama kategori',
+            inputValue: name,
+            inputPlaceholder: 'Masukan nama kategori yang baru',
+            confirmButtonText: 'Ubah',
+            showCancelButton: true,
+            showLoaderOnConfirm: true,
+            preConfirm: async (value) => {
+                const { error } = await supabase
+                    .from('categories')
+                    .update({ name: value })
+                    .eq('id', id)
+
+                if (!error) {
+                    Swal.hideLoading()
+                    return true
+                }
+                Swal.showValidationMessage(ERROR_MESSAGE)
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire('Berhasil', 'Kategori di ubah', 'success')
+                getCategories()
+            }
+        });
+
+    }
     useEffect(() => {
         getCategories()
     }, [])
@@ -62,43 +128,14 @@ export default function Category() {
     return (isLoading ? <Loader loadText={'Mohon tunggu...'} /> :
         <div>
             <div className='grid grid-cols-1 md:grid-cols-2'>
-                <h1>Kategori</h1>
+                <h1 className='text-2xl'>Kategori</h1>
                 <div className='flex justify-end'>
-                    <ButtonComponent text={'TAMBAH'} className={`md:w-1/4 w-full`} onClick={() => setModalShow(true)} />
+                    <ButtonComponent text={'TAMBAH'} className={`md:w-1/4 w-full`} onClick={() => createCategory()} />
                 </div>
             </div>
             <div className='w-full'>
-                <TableComponent data={data} heads={heads} />
+                <TableComponent data={categoriesList} heads={heads} />
             </div>
-            <Modal
-                show={modalShow}
-                size="md"
-                popup={true}
-                onClose={() => setModalShow(false)}
-            >
-                <Modal.Header />
-                <Modal.Body>
-                    <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
-                        <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-                            Masukan data kategori
-                        </h3>
-                        <form ref={formRef} onSubmit={e => {
-                            e.preventDefault()
-                            let formData = new FormData(e.target)
-                            let name = formData.get('name')
-                            addCategory(name)
-                            formRef.current.reset()
-                        }}>
-                            <div className='w-full'>
-                                <InputComponent autoFocus={true} className={`w-full`} name={`name`} placeholder={`Masukan nama kategori`} />
-                            </div>
-                            <div className="w-full">
-                                <ButtonComponent className={'w-full'} text={`TAMBAH`} onClick={() => { }} />
-                            </div>
-                        </form>
-                    </div>
-                </Modal.Body>
-            </Modal>
         </div>
     )
 }
